9bc37df1732e1280d9343436d2ae596806e222d8
lav3ll <lavell.francis@hotmail.com>
2024-02-07 19:59:27 +0000

    installed tailwind,react and nivo dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <>HELLO WORLD</>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
