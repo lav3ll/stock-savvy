9a3f5366b7bf7dfcce4e61997ec0a8212834c2c4
lav3ll <lavell.francis@hotmail.com>
2024-02-08 20:04:12 +0000

    Added comments to components for better readability

--- src/components/TopPageStockData/StockData.jsx
+++ src/components/TopPageStockData/StockData.jsx
@@ -7,13 +7,17 @@ const StockData = ({
   change_percentage,
   volume,
 }) => {
+  // Convert change percentage and change amount to numbers from strings
   const formattedChangePercentage = parseFloat(change_percentage);
   const formattedChangeAmount = parseFloat(change_amount);
 
   return (
     <div>
+      {/* Display the ticker symbol */}
       <h1 className="text-lg font-bold">{ticker}</h1>
+      {/* Display the price */}
       <p className="text-gray-600">Price: {price}</p>
+      {/* Apply text color class based on whether change amount is positive or negative */}
       <p
         className={`${
           formattedChangeAmount > 0 ? "text-green-600" : "text-red-600"
@@ -21,6 +25,7 @@ const StockData = ({
       >
         {formattedChangeAmount}
       </p>
+      {/* Apply text color class based on whether change percentage is positive or negative */}
       <p
         className={`text-${
           formattedChangePercentage >= 0 ? "green" : "red"
@@ -28,6 +33,7 @@ const StockData = ({
       >
         {formattedChangePercentage}%
       </p>
+      {/* Display the volume */}
       {/* <p className="text-gray-600">Volume: {volume}</p> */}
     </div>
   );
--- src/components/TopPageStockData/StocksLanding.jsx
+++ src/components/TopPageStockData/StocksLanding.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import StockData from "./StockData";
-import stocks from "../data/gainers-losers-activelydata.json";
+import StockData from "./StockData"; // Importing the StockData component
+import stocks from "../data/gainers-losers-activelydata.json"; // Importing the stock data JSON file
 
+// Extracting the most actively traded stocks from the imported JSON data
 const mostActivelyTraded = stocks.most_actively_traded;
-const topGainers = stocks.top_gainers;
-const topLosers = stocks.top_losers;
 
+// Functional component for rendering the stocks landing page
 export const StocksLanding = () => {
   return (
     <div>
+      {/* Container div with flex layout */}
       <div className="flex justify-between items-center border-b border-gray-200 py-2">
+        {/* Mapping over the most actively traded stocks and rendering StockData component for each */}
         {mostActivelyTraded.map((stock, idx) => (
           <StockData
             key={idx}
