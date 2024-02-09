3bda1fd5e9f3d0e83e67df1f3a4fe1ba5fbce771
Alex Christakou <alex@Alexs-Air-2.broadband>
2024-02-09 19:51:24 +0000

    build CSS and React Grid and template

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import "./App.css";
 import { StocksLanding } from "./components/TopPageStockData/StocksLanding";
 import GLContainer from "./components/SideStockData/GLContainer";
+import Dashboard from "./components/dashboard";
 
 function App() {
   return (
     <>
+      <Dashboard />
       <StocksLanding />
       <GLContainer />
     </>
--- src/components/dashboard.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Card from "./Card";
-
-const Dashboard = () => {
-    return <div></div>
-};
-
-export default Card; 
