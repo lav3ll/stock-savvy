2c69588d8e16fd9e75d930285660838dcfc460c3
Thomas Gledhill <thomaspg@live.co.uk>
2024-02-12 15:33:47 +0000

    reset navbar due to incompatibility

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import { useState } from "react";
 import "./App.css";
 import { StocksLanding } from "./components/TopPageStockData/StocksLanding";
 import GLContainer from "./components/SideStockData/GLContainer";
-import Navbar from "./components/Navbar/Navbar"
+
 
 function App() {
   return (
     <>
-      <Navbar />
       <StocksLanding />
       <GLContainer />
     </>
