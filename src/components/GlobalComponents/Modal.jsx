29d55dc06efa3beb88c890f6591630cae4020d91
lav3ll <lavell.francis@hotmail.com>
2024-02-12 20:55:32 +0000

    line chart now renders but missing data

--- src/components/GlobalComponents/Modal.jsx
+++ src/components/GlobalComponents/Modal.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import LineChart from "./LineChart";
 
 const Modal = ({ isOpen, onClose, data, modalBtnTxt }) => {
   return (
@@ -43,11 +44,14 @@ const Modal = ({ isOpen, onClose, data, modalBtnTxt }) => {
                 <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
                   {data.Industry}
                 </p>
+                <p></p>
               </div>
+
               {/* Modal footer */}
               <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600"></div>
             </div>
           </div>
+          <LineChart stockData={data} />
         </div>
       )}
     </>
