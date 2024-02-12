cf79907f766eb16e4701d34cf7bb0485bdc224b4
lav3ll <lavell.francis@hotmail.com>
2024-02-12 22:13:12 +0000

    updated colors for bar chart

--- src/components/GlobalComponents/BarChart.jsx
+++ src/components/GlobalComponents/BarChart.jsx
@@ -1,45 +1,94 @@
-import React from "react";
 import { ResponsiveBar } from "@nivo/bar";
+import NivoTheme from "../data/NivoTheme.json";
 
 const BarChart = ({ stockData }) => {
   const data = [
     {
       metric: "Market Capitalization",
-      value: parseFloat(stockData.MarketCapitalization),
+      MarketCapitalization: parseFloat(stockData.MarketCapitalization),
     },
-    { metric: "Revenue (TTM)", value: parseFloat(stockData.RevenueTTM) },
-    { metric: "Earnings Per Share (EPS)", value: parseFloat(stockData.EPS) },
+    {
+      metric: "EBITDA",
+      EBITDA: parseFloat(stockData.EBITDA),
+    },
+    {
+      metric: "Revenue (TTM)",
+      RevenueTTM: parseFloat(stockData.RevenueTTM),
+    },
+    {
+      metric: "Gross Profit (TTM)",
+      GrossProfitTTM: parseFloat(stockData.GrossProfitTTM),
+    },
+    {
+      metric: "Shares Outstanding",
+      SharesOutstanding: parseFloat(stockData.SharesOutstanding),
+    },
   ];
 
   return (
     <ResponsiveBar
       data={data}
-      keys={["value"]}
+      keys={[
+        "MarketCapitalization",
+        "EBITDA",
+        "RevenueTTM",
+        "GrossProfitTTM",
+        "SharesOutstanding",
+      ]}
       indexBy="metric"
-      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+      margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
       padding={0.3}
-      colors={{ scheme: "category10" }}
+      layout="horizontal"
+      colors={{ scheme: "accent" }}
       axisTop={null}
       axisRight={null}
       axisBottom={{
         tickSize: 5,
         tickPadding: 5,
         tickRotation: -45,
+        legend: "Value",
+        legendPosition: "middle",
+        legendOffset: 36,
       }}
       axisLeft={{
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: "Value",
+        legend: "Metrics",
         legendPosition: "middle",
         legendOffset: -40,
       }}
       labelSkipWidth={12}
       labelSkipHeight={12}
       labelTextColor={{ from: "color", modifiers: [["darker", 1.6]] }}
+      legends={[
+        {
+          dataFrom: "keys",
+          anchor: "bottom-right",
+          direction: "column",
+          justify: false,
+          translateX: 120,
+          translateY: 0,
+          itemsSpacing: 2,
+          itemWidth: 100,
+          itemHeight: 20,
+          itemDirection: "left-to-right",
+          itemOpacity: 0.85,
+          symbolSize: 20,
+          effects: [
+            {
+              on: "hover",
+              style: {
+                itemOpacity: 1,
+              },
+            },
+          ],
+        },
+      ]}
       animate={true}
       motionStiffness={90}
       motionDamping={15}
+      // theme={NivoTheme}
     />
   );
 };
