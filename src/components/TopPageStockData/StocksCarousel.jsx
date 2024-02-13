d219d0bb30a1cbe94d97fd5a4e2d497d9349ec96
lav3ll <lavell.francis@hotmail.com>
2024-02-13 13:45:26 +0000

    duplicated the items in the carousel to cover for the gap

--- src/components/TopPageStockData/StocksCarousel.jsx
+++ src/components/TopPageStockData/StocksCarousel.jsx
@@ -34,6 +34,16 @@ const StocksCarousel = () => {
             volume={stock.volume}
           />
         ))}
+        {stocks.most_actively_traded.map((stock, idx) => (
+          <StockData
+            key={idx}
+            ticker={stock.ticker}
+            price={stock.price}
+            change_amount={stock.change_amount}
+            change_percentage={stock.change_percentage}
+            volume={stock.volume}
+          />
+        ))}
       </div>
     </div>
   );
