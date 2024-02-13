27d32b875e339158e372de6937962e6f281d12fb
lav3ll <lavell.francis@hotmail.com>
2024-02-13 00:40:25 +0000

    added some comments for readability

--- src/components/TopPageStockData/StocksCarousel.jsx
+++ src/components/TopPageStockData/StocksCarousel.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import StockData from "./StockData";
 import stocks from "../data/gainers-losers-activelydata.json";
+import "./StocksCarousel.scss";
 
 const StocksCarousel = () => {
+  const carouselRef = useRef(null);
+
+  // Function to scroll the carousel
+  const scrollCarousel = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollLeft += 1; //scroll speed
+    }
+  };
+
+  // Automatically scroll the carousel
+  useEffect(() => {
+    const intervalId = setInterval(scrollCarousel, 50); //  interval duration
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     // Container for the carousel with overflow hidden to hide overflowing content
     <div className="overflow-hidden carousel-container">
       {/* Flex container for the carousel items with specified gap and padding */}
-      <div className="flex gap-4 py-2">
-        {/* Mapping over the most actively traded stocks to render each as a StockData component */}
+      <div ref={carouselRef} className="flex gap-4 py-2 carousel-track">
         {stocks.most_actively_traded.map((stock, idx) => (
           <StockData
             key={idx}
