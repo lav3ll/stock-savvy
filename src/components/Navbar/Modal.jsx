ed9861917e805f17f87bf36da46d9a45e72bdab3
Thomas Gledhill <thomaspg@live.co.uk>
2024-02-14 17:15:46 +0000

    add styling to be converted to tailwind

--- src/components/Navbar/Modal.jsx
+++ src/components/Navbar/Modal.jsx
@@ -14,14 +14,19 @@ export default function Modal() {
         <div className="div-modal" onClick={toggleModal}>
             Contact Us
         </div>
+
+    {modal && (
         <div className="my-modal">
             <div className="my-overlay" onClick={toggleModal}></div>
-            <div className="modal-content">
-                <h2>Contact Us</h2>
-                <p>Contact us here this is all our infodhfgidogmwigowi</p>
+            <div className="modal-content text-black">
+                <h2 className="modal-header">Contact Us</h2>
+                <p><b>Email: </b>customersupport@stocksavvy.com</p>
+                <p><b>Phone: </b>01483 700700</p>
+                <p><b>Intsagram: </b>@stock_savvy</p>
                 <button className="close-btn" onClick={toggleModal}>Close</button>
             </div>
-        </div>
+        </div>  
+    )}
         </>
 
     )
