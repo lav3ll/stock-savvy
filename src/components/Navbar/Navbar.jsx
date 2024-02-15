aa8f5cd00559e5b104b266d6f58a751e59e4b645
Alex Christakou <alex@Alexs-Air-2.broadband>
2024-02-15 19:19:48 +0000

    made navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,16 +8,16 @@ export default function Navbar({news, setNews, setFiftyDayAverage, setStockName,
 const [results, setResults] = useState([])
 
     return (
-        <nav className="my-nav bg-indigo-900 text-white flex justify-between items-center gap-8 p-4">
-            <div className="flex gap-5 my-logo flex-grow">
+        <nav className="bg-indigo-900 text-white flex justify-between items-center gap-2 p-2 md:gap-8 md:p-4">
+            <div className="flex gap-2 md:gap-5 my-logo flex-grow">
                 <img src="/stocksavvy-logo-1.jpg" alt="" className="h-10 rounded-xl"/>
-                <a href="/" className="my-site text-3xl">Stock Savvy</a>
+                <a href="/" className="text-xl md:text-3xl lg:flex hidden">Stock Savvy</a>
             </div>
-            <div className="searchbar-container flex-grow">
+            <div className="searchbar-container w-full md:w-auto">
                 <Searchbar news={news} setNews={setNews} setResults={setResults}/>
                 <SearchResultsList results={results} setFiftyDayAverage={setFiftyDayAverage} setStockName={setStockName} setMarketCap={setMarketCap} setExchange={setExchange} />
             </div>
-            <ul className="flex gap-8 flex-grow">
+            <ul className="flex gap-2 md:gap-8">
                 <li><a href="/">Home</a></li>
                 <li><Modal></Modal></li>
             </ul>
